e3ea35e5cb3b1d660f3cde806b04691108e5e145
Pragyasmita <sprajnasmita@gmail.com>
2023-05-13 22:57:44 +0530

    first commit

---
+++ src/defaultpackage/Test1.java
@@ -0,0 +1,17 @@
+package defaultpackage;
+
+public class Test1 {
+
+		public static void main(String[] args) {
+			String hungry="yes";
+			if(hungry=="yes") {
+				System.out.println("Eat something.......");
+			}
+				else {
+					System.out.println("do work........");
+				}
+			}
+
+		}
+
+
